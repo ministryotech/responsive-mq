549674684452d68053df19dd9d379a545102b332
Keith Jackson <temporal-net@live.co.uk>
2023-12-30 19:09:52 +0000

    New version

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # responsive-mq.js #
 
 ### What is it for? ###
-Responsive MQ has been designed to enable you to attach JavaScript functions to media queries so you can have viewport specific code executing within your app to partner up with your responsive CSS. It does this by registering named elements of markup which can then be checked on resize to determine what the current viewport is. This is handled by the responsive-mq.js 'registerBreakpoint()' and 'activate()' functions.
+Responsive MQ has been designed to enable you to attach JavaScript functions to media queries, so you can have viewport specific code executing within your app to partner up with your responsive CSS. It does this by registering named elements of markup which can then be checked on resize to determine what the current viewport is. This is handled by the responsive-mq.js 'registerBreakpoint()' and 'activate()' functions.
 
 ### How do I get set up? ###
 In order to make use of Responsive MQ, simply add a reference to the javascript file in your page or using your favourite script loader.
@@ -21,10 +21,10 @@ As of v 1.2.2 it now supports AMD - You can use it with RequireJS like this...
 
 This will affect where Responsive MQ is placed. If you are using traditional methodology then it will be attached to the jQuery object. If you are using RequireJS then it will be returned by itself.
 
-Responsive MQ is dependent on jQuery. It's been tested with 1.11 but should be compatible with all versions of jQuery - Feel free to raise a bug if you experience any issues with specifc versions. The reason for this is jQuery's comparatively faster speed when setting up resize event responses.
+Responsive MQ is dependent on jQuery. It's been tested with 1.11 but should be compatible with all versions of jQuery - Feel free to raise a bug if you experience any issues with specific versions. The reason for this is jQuery's comparatively faster speed when setting up resize event responses.
 
 ### How do I use it? ###
-Using responsive-mq is a two step process. Firstly you need to register a breakpoint against a matching function and an optional function to execute when the breakpoint no longer matches the media query...
+Using responsive-mq is a two-step process. Firstly you need to register a breakpoint against a matching function and an optional function to execute when the breakpoint no longer matches the media query...
 
 	$.responsiveMQ.registerBreakpoint('mobile', '', function () {
 	    $('#display').text('This is mobile viewport');
@@ -43,29 +43,28 @@ Once you have registered all of your breakpoint functions you can activate respo
 
 	$.responsiveMQ.activate();
 
-This needs to be a multi-step process so that it can render all of the required markup and style blocks.
+This needs to be a multistep process so that it can render all the required markup and style blocks.
 
 ### Limitations ###
 The intention of responsive-mq is purely to handle identification of a single valid viewport. To this end only one of the specified registered breakpoints will be active at any one time. Trying to use multiple overlapping media queries will have unpredictable results.
 
-## The Ministry of Technology Open Source Products ##
-Welcome to The Ministry of Technology open source products. All open source Ministry of Technology products are distributed under the MIT License for maximum re-usability. Details on more of our products and services can be found on our website at http://www.minotech.co.uk
-
+# The Ministry of Technology Open Source Products
+Welcome to The Ministry of Technology open source products. All open source Ministry of Technology products are distributed under the MIT License for maximum re-usability.
 Our other open source repositories can be found here...
 
 * [https://github.com/ministryotech](https://github.com/ministryotech)
 * [https://github.com/tiefling](https://github.com/tiefling)
 
 ### Where can I get it? ###
-You can download the javascript file (dev or minified version) from the downloads page here and add it to your website manually or you can use any of the following package managers...
+You can download the javascript file (dev or minified version) from the downloads page here and add it to your website manually, or you can use any of the following package managers...
 
 - **NPM** - [https://www.npmjs.org/package/responsive-mq](https://www.npmjs.org/package/responsive-mq)
-- **NUGET** - [https://www.nuget.org/packages/responsive-mq/](https://www.nuget.org/packages/responsive-mq/)
+- Nuget is no longer supported
 
 ### Contribution guidelines ###
 If you would like to contribute to the project, please contact me.
 
-The source code can be used in a simple text editor or within Visual Studio using NodeJS Tools for Visual Studio.
+The source code can be used in a simple text editor or within Visual Studio using Node.js Tools for Visual Studio.
 
 ### Who do I talk to? ###
-* Keith Jackson - keith@minotech.co.uk
+* Keith Jackson - temporal-net@live.co.uk
--- responsive-mq.js
+++ responsive-mq.js
@@ -1,34 +1,38 @@
-﻿/* responsive-mq.js - v1.2.4 */
+﻿/* responsive-mq.js - v1.3.0 */
 /*
  * ResponsiveMQ     Bringing media queries to Javascript
  * Author           Keith Jackson (tiefling)
  * Company          The Ministry of Technology (ministryotech)
  * License          MIT / http://bit.ly/mit-license
- * WebSite          http://www.ministryotech.co.uk
  */
 
-var respMqFunc = function ($) {
+const respMqFunc = function ($) {
 
-    var responsiveMq = {};
+    const responsiveMq = {};
 
     responsiveMq.registeredBreakpoints = [];
     responsiveMq.lastActiveBreakpointName = '';
 
     responsiveMq.registerBreakpoint = function (name, mediaQueryCssName, matchedFunc, notMatchedFunc) {
-        responsiveMq.registeredBreakpoints.push({ 'name': name, 'mediaQueryCssName': mediaQueryCssName, 'matchedFunc': matchedFunc, 'notMatchedFunc': notMatchedFunc });
+        responsiveMq.registeredBreakpoints.push({
+            'name': name,
+            'mediaQueryCssName': mediaQueryCssName,
+            'matchedFunc': matchedFunc,
+            'notMatchedFunc': notMatchedFunc
+        });
     };
 
     responsiveMq.activate = function () {
 
-        var renderMarkup = function (className) {
+        const renderMarkup = function (className) {
             $(document.body).append('<span class="' + className + '"></span>');
         };
 
-        var renderStyle = function (className, mediaQueryCssName) {
-            var styleString = '\n';
+        const renderStyle = function (className, mediaQueryCssName) {
+            let styleString = '\n';
             if (mediaQueryCssName !== '') {
                 styleString += mediaQueryCssName + '{\n';
-                for (var bpi = 0; bpi < responsiveMq.registeredBreakpoints.length; bpi++) {
+                for (let bpi = 0; bpi < responsiveMq.registeredBreakpoints.length; bpi++) {
                     if (responsiveMq.registeredBreakpoints[bpi].name !== className) {
                         styleString += '   .' + responsiveMq.registeredBreakpoints[bpi].name + ' { display: none; }\n';
                     } else {
@@ -37,7 +41,7 @@ var respMqFunc = function ($) {
                 }
                 styleString += '}\n';
             } else {
-                for (var bpx = 0; bpx < responsiveMq.registeredBreakpoints.length; bpx++) {
+                for (let bpx = 0; bpx < responsiveMq.registeredBreakpoints.length; bpx++) {
                     if (responsiveMq.registeredBreakpoints[bpx].name !== className) {
                         styleString += '.' + responsiveMq.registeredBreakpoints[bpx].name + ' { display: none; }\n';
                     } else {
@@ -48,19 +52,19 @@ var respMqFunc = function ($) {
             $(document.head).append('<style>' + styleString + '</style>');
         };
 
-        var applyResponsiveJs = function () {
-            for (var rjsi = 0; rjsi < responsiveMq.registeredBreakpoints.length; rjsi++) {
-                var bp = responsiveMq.registeredBreakpoints[rjsi];
-                if ($('.' + bp.name).css('display') == 'inline') {
+        const applyResponsiveJs = function () {
+            for (let rjsi = 0; rjsi < responsiveMq.registeredBreakpoints.length; rjsi++) {
+                const bp = responsiveMq.registeredBreakpoints[rjsi];
+                if ($('.' + bp.name).css('display') === 'inline') {
                     if (bp.name !== responsiveMq.lastActiveBreakpointName) {
                         responsiveMq.lastActiveBreakpointName = bp.name;
-                        var matchedFunc = bp.matchedFunc;
+                        const matchedFunc = bp.matchedFunc;
                         if (matchedFunc !== undefined && matchedFunc !== null) {
                             matchedFunc();
                         }
                     }
                 } else {
-                    var notMatchedFunc = bp.notMatchedFunc;
+                    const notMatchedFunc = bp.notMatchedFunc;
                     if (notMatchedFunc !== undefined && notMatchedFunc !== null) {
                         notMatchedFunc();
                     }
@@ -68,7 +72,7 @@ var respMqFunc = function ($) {
             }
         };
 
-        for (var i = 0; i < responsiveMq.registeredBreakpoints.length; i++) {
+        for (let i = 0; i < responsiveMq.registeredBreakpoints.length; i++) {
             renderMarkup(responsiveMq.registeredBreakpoints[i].name);
             renderStyle(responsiveMq.registeredBreakpoints[i].name, responsiveMq.registeredBreakpoints[i].mediaQueryCssName);
         }
@@ -80,8 +84,15 @@ var respMqFunc = function ($) {
     return responsiveMq;
 };
 
+/*--------------------------------------------------------------------------*/
+
+// Export library
+// noinspection JSUnresolvedReference - define check for require.js module support.
 if (typeof define === 'function' && define.amd) {
+    // noinspection JSUnresolvedReference - define check for require.js module support.
     define(['jquery'], respMqFunc);
+} else if (typeof exports === 'object') {
+    module.exports = respMqFunc
 } else {
     $.responsiveMQ = respMqFunc(jQuery);
 }
